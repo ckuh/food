675c088e1db63035245366640737b31fb826b1a7
Chris Kuh <kuh_chris@yahoo.com>
2016-09-26 13:33:35 -0700

    [update] changed text for bio

--- js/About.jsx
+++ js/About.jsx
@@ -11,7 +11,7 @@ const About = () => (
       <h3>Hey there,</h3>
       <h1>I'm <span>Chris Kuh</span></h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Hi my name is Chris Kuh. I Previously programmed microcontrollers in C. While doing this I learned that I had a passion for programming. Although programming microcontrollers was exciting, I wanted to expand my tool set and build applications that anyone could use. Therefor, I partook in an accelerated learning program for JavaScript. During this program I honed my skills in the MEAN stack and now more then confident in building any application.
       </p>
       <div>
         <Button href='https://github.com/ckuh' bsClass='btn about-button'>Github</Button>
